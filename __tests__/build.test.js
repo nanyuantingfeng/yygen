50afe462d64b9f41dd6dc0973c28a632f2197c0e
nanyuantingfeng <nanyuantingfeng@sina.com>
2018-08-10 13:37:20 +0800

    update test files

--- __tests__/build.test.js
+++ __tests__/build.test.js
@@ -35,3 +35,88 @@ describe('build', () => {
     })
   })
 })
+
+describe('build none output', () => {
+  const table = [
+    { types: ['ts'], name: 'demo-demo-0' },
+    {
+      types: ['ts', 'ekuaibao'],
+      name: 'demo-demo-1'
+    },
+    {
+      types: ['ts', 'ekuaibao', 'lint'],
+      name: 'demo-demo-2'
+    },
+    {
+      types: ['ts', 'ekuaibao', 'lint', 'jest'],
+      name: 'demo-demo-3'
+    },
+    {
+      types: ['ekuaibao', 'lint', 'jest'],
+      name: 'demo-demo-4'
+    }
+  ]
+
+  describe.each(table)('.each(%j)', config => {
+    test(`show be types: ${config.types.join(',')}`, async () => {
+      const ctx = await build(config)
+      expect(ctx).toMatchSnapshot()
+    })
+  })
+})
+
+describe('build none name', () => {
+  const table = [
+    { types: ['ts'], output: './demo_demo_0' },
+    {
+      types: ['ts', 'ekuaibao'],
+      output: 'demo_demo-1'
+    },
+    {
+      types: ['ts', 'ekuaibao', 'lint'],
+      output: 'demo_demo-2'
+    },
+    {
+      types: ['ts', 'ekuaibao', 'lint', 'jest'],
+      output: 'demo_demo-3'
+    },
+    {
+      types: ['ekuaibao', 'lint', 'jest'],
+      output: 'demo_demo-4'
+    }
+  ]
+
+  describe.each(table)('.each(%j)', config => {
+    test(`show be types: ${config.types.join(',')}`, async () => {
+      const ctx = await build(config)
+      expect(ctx).toMatchSnapshot()
+    })
+  })
+})
+
+describe('build none name & output', () => {
+  const table = [
+    {
+      types: ['ts']
+    },
+    {
+      types: ['ts', 'ekuaibao']
+    },
+    {
+      types: ['ts', 'ekuaibao', 'lint']
+    },
+    {
+      types: ['ts', 'ekuaibao', 'lint', 'jest']
+    },
+    {
+      types: ['ekuaibao', 'lint', 'jest']
+    }
+  ]
+
+  describe.each(table)('.each(%j)', config => {
+    test(`show be types: ${config.types.join(',')}`, async () => {
+      const ctx = await build(config)
+      expect(ctx).toMatchSnapshot()
+    })
+  })
+})
